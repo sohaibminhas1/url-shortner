9464ba36576572cdd73e282f9daa811b3d32d61b
Sohaib Minhas <sohaibminhas12@gmail.com>
2025-08-15 01:14:14 +0500

    Updated role check logic in auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,8 @@
 const {getUser} = require("../service/auth");
 
 
-function checkForAuthentication(req,res,next){
+function checkForAuthentication(req,res,next)
+{
     const tokenCookie = req.cookies.token;
     req.user = null;
 
@@ -15,27 +16,21 @@ function checkForAuthentication(req,res,next){
     return next()
 }
 
-function restrictTo(roles = []){
-    return function (req,res,next){
-        console.log("Roles Allowed", roles);
-        console.log("User Object", req.user);
+function restrictTo(roles = [])
+{
+    return function (req,res,next)
+    {    
         if(!req.user)
-            {
-        return res.redirect("/login");
-        
-        }
-         if (!Array.isArray(roles)) roles = [roles]
         {
-         console.log(`Role '${req.user.role}' is not allowed`);
+        return res.redirect("/login"); 
         }
         if(!roles.includes(req.user.role))
         {
-            return res.end("UnAuthorized Access");
+        return res.end("UnAuthorized Access");
         }
         console.log("CHECK PASSED")
 
-        return next();
-            
+        return next();       
     }
 }
 
